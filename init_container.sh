50f1d4e12f5c7770bde7592a54eba251ee766728
Amin Kashiri <kashiri.amin@gmail.com>
2025-09-08 20:52:23 +0000

    Update inti container bash script

--- init_container.sh
+++ init_container.sh
@@ -2,6 +2,7 @@
 
 # Only needed if you don't use docker file. You can create a container with image: fairembodied/habitat-challenge:homerobot-ovmm-challenge-2023 
 # and run this script inside the container.
+# docker run -it --gpus all -v ~/Workspace/exploration/hm3d_dataset/hm3d-0.2:/home-robot/data/versioned_data fairembodied/habitat-challenge:homerobot-ovmm-challenge-2023 bash
 
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh |  bash
 apt install git-lfs
@@ -47,5 +48,11 @@ unzip goat-bench.zip
 mv data/datasets/goat_bench data/datasets/goat_openvocab 
 mv data/datasets/goat_openvocab/hm3d/v1 data/datasets/goat_openvocab/hm3d/v0.1.2_fixed 
 
+# Either mount the dataset, or download it:
+mkdir data/scene_datasets
+ln -s /home-robot/data/versioned_data/hm3d-0.2/hm3d data/scene_datasets/hm3d 
 
-yes | python -m habitat_sim.utils.datasets_download --username 2523e65432178e8d --password c5cd9238658cf8c9c81676965a49b859 --uids hm3d_full
+git fetch --all
+git checkout goat_fixed
+
+# yes | python -m habitat_sim.utils.datasets_download --username 2523e65432178e8d --password c5cd9238658cf8c9c81676965a49b859 --uids hm3d_full
